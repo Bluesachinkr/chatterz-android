5c8d24b4615625231bda7f423bedaadf698c2f45
Sachin Kumar <schnkr98@gmail.com>
2019-12-25 18:35:30 +0530

    Firebase Group Model classes

--- app/src/main/java/com/zone/chatterz/Adapter/GroupAdapter.kt
+++ app/src/main/java/com/zone/chatterz/Adapter/GroupAdapter.kt
@@ -1,26 +1,31 @@
 package com.zone.chatterz.Adapter
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mikhaellopez.circularimageview.CircularImageView
-import com.zone.chatterz.GroupChatActivity
 import com.zone.chatterz.Model.Group
 import com.zone.chatterz.R
 
-class GroupAdapter(context: Context,list: List<Group>) : RecyclerView.Adapter<GroupAdapter.ViewHolder>(){
+class GroupAdapter(context: Context, list: List<Group>) :
+    RecyclerView.Adapter<GroupAdapter.ViewHolder>() {
 
     private val mContext = context
     private val mGroup = list
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var groupCard: CardView = itemView.findViewById(R.id.GroupCard)
+        var groupImage: CircularImageView = itemView.findViewById(R.id.Group_profileImage)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(mContext).inflate(R.layout.group_item, parent, false)
-            return ViewHolder(view)
+        val view = LayoutInflater.from(mContext).inflate(R.layout.group_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -29,20 +34,19 @@ class GroupAdapter(context: Context,list: List<Group>) : RecyclerView.Adapter<Gr
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var group = mGroup.get(position)
-        holder.groupName.text = group.groupName
-        if(group.groupImgUrl.equals("default")){
+        if (group.groupImgUrl.equals("default")) {
             holder.groupImage.setImageResource(R.mipmap.ic_launcher_round)
-        } else{
+        } else {
             Glide.with(mContext).load(group.groupImgUrl).into(holder.groupImage)
         }
+        holder.groupCard.visibility = View.GONE
+        /*holder.itemView.setOnClickListener{
+            LoadGroupData()
+        }*/
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
-        var groupName = itemView.findViewById<TextView>(R.id.GroupName)
-        var groupLastMessage = itemView.findViewById<TextView>(R.id.messageGroup)
-        var groupImage = itemView.findViewById<CircularImageView>(R.id.Group_profileImage)
-        var lastMessageStatus = itemView.findViewById<TextView>(R.id.statusLastMessage)
+    private fun LoadGroupData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
--- app/src/main/java/com/zone/chatterz/GroupChatActivity.kt
+++ app/src/main/java/com/zone/chatterz/GroupChatActivity.kt
@@ -1,28 +1,32 @@
 package com.zone.chatterz
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 import com.zone.chatterz.Interfaces.DrawerLocker
 
-open class GroupChatActivity : Fragment(),View.OnClickListener {
+open class GroupChatActivity : Fragment(), View.OnClickListener {
 
-    private lateinit var drawer : DrawerLayout
+    private lateinit var drawer: DrawerLayout
     private lateinit var leftNavigationView: NavigationView
-    private lateinit var openDrawerBtn : ImageView
-    private lateinit var openMembersDrawer : ImageView
-    private lateinit var groupName : TextView
-    private lateinit var content : RelativeLayout
+    private lateinit var openDrawerBtn: ImageView
+    private lateinit var openMembersDrawer: ImageView
+    private lateinit var groupName: TextView
+    private lateinit var content: RelativeLayout
     private lateinit var rightNavigationView: NavigationView
+    private lateinit var groupListView: RecyclerView
+    private lateinit var toolbar: Toolbar
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +35,7 @@ open class GroupChatActivity : Fragment(),View.OnClickListener {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
 
-        val view = inflater.inflate(R.layout.activity_group_chat,container,false)
+        val view = inflater.inflate(R.layout.activity_group_chat, container, false)
         drawer = view.findViewById(R.id.drawerGroups)
         leftNavigationView = view.findViewById(R.id.drawerOpen)
         rightNavigationView = view.findViewById(R.id.drawerMembers)
@@ -39,36 +43,61 @@ open class GroupChatActivity : Fragment(),View.OnClickListener {
         openMembersDrawer = view.findViewById(R.id.membersBtn)
         groupName = view.findViewById(R.id.groupName)
         content = view.findViewById(R.id.contentGroupChats)
+        //  groupListView = leftNavigationView.findViewById(R.id.GroupsList)
+        toolbar = view.findViewById(R.id.groupToolbar)
+
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
         (activity as DrawerLocker).setDrawerLockerEnabled(false)
 
         drawer.setScrimColor(Color.TRANSPARENT)
 
         setDrawerWidth()
-        drawer.addDrawerListener(object : ActionBarDrawerToggle(this.activity,drawer,R.string.openDrawer,R.string.closeDrawer){
+        drawer.addDrawerListener(object : ActionBarDrawerToggle(
+            this.activity,
+            drawer,
+            R.string.openDrawer,
+            R.string.closeDrawer
+        ) {
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
                 super.onDrawerSlide(drawerView, slideOffset)
                 val width = drawerView.width * slideOffset
-                if(drawerView!=rightNavigationView) {
+                if (drawerView != rightNavigationView) {
                     content.translationX = width
                 }
             }
         })
-
-
 
         openDrawerBtn.setOnClickListener(this)
         openMembersDrawer.setOnClickListener(this)
         return view
     }
 
-    override fun onClick(v: View?) {
-        when(v){
-            openDrawerBtn->{
-                drawer.openDrawer(leftNavigationView)
-               //showGroups()
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.group_menu_appbar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.addNewgroup -> {
+                val intetnt = Intent(this.activity, CreateNewGroup::class.java)
+                startActivity(intetnt)
+                return true
             }
-            openMembersDrawer->{
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            openDrawerBtn -> {
+                drawer.openDrawer(leftNavigationView)
+                //showGroups()
+            }
+            openMembersDrawer -> {
                 drawer.openDrawer(rightNavigationView)
                 //showMembers()
             }
